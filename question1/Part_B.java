91ccadbeba074ab2f50906c293374fcd22f7caf3
IdkMyUsername2 <chris_737@icloud.com>
2026-05-21 09:58:08 -0400

    Update Part_B.java

--- question1/Part_B.java
+++ question1/Part_B.java
@@ -1,1 +1,8 @@
-
+public String getShortenedName(){
+	String temp = username; 
+	while(temp.indexOf("-")){
+		int num = temp.indexOf("-");
+		temp = temp.substring(0, num-1) + tmep.substring(num+1);
+	}
+	return temp;	
+}
