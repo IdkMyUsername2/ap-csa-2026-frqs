098dbebe13a80078f67a766c2a91423fc223fc6f
IdkMyUsername2 <chris_737@icloud.com>
2026-05-26 02:43:12 -0400

    Update Solution.java

--- question4/Solution.java
+++ question4/Solution.java
@@ -1,9 +1,19 @@
 public int getPointsForRow(int targetRow){
-	for(int i = 0; i < board[targetRow].length; i++){
-	
+	int total = board[targetRow][0].getPoints():
+	String firstColor= board[targetRow][0].getColor();
+	boolean bleh = true;
+	for(int i = 1; i < board[targetRow].length; i++){
+		total = board[targetRow][i].getPoints();
+		if(! prevColor.equals(board[targetRow][i].getColor())){
+			bleh = false;
+		}
 	}
 
+	if(bleh){
+		return total * 2;
+	}
 
+	return total;
 
 	
 }
