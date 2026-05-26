c32d87bec1ae578739d961940264ec34dc1b63b5
IdkMyUsername2 <chris_737@icloud.com>
2026-05-26 02:23:37 -0400

    Update Solution.java

--- question2/Solution.java
+++ question2/Solution.java
@@ -1,1 +1,20 @@
+public class bottle{
+	private double startAmount
+	private double amount;
+	private double minAmount;
 
+	public bottle(double startAmount){
+		this.startAmount = startAmount;
+		amount = a;
+		minAmount = 0.25 * a;
+	}
+
+	public double updateAmount(double remove){
+		amount - remove;
+		if(amount < minAmount){
+			amount = startAmount;
+		}
+		return startAmount;
+	}
+	
+}
