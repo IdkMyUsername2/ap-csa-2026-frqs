37cc6e34f7b1f30ed55644e2b899ef445a158f09
IdkMyUsername2 <chris_737@icloud.com>
2026-05-26 02:37:59 -0400

    Update Solution.java

--- question3/Solution.java
+++ question3/Solution.java
@@ -1,7 +1,7 @@
 public int moreHistoryThanMathAbsences(){
 	int total = 0;
-	for(int i = 0; i < historyList.length(); i++){
-		for(int z = 0; z < mathList.length(); z++){
+	for(int i = 0; i < historyList.size(); i++){
+		for(int z = 0; z < mathList.size(); z++){
 			if(historyList.get(i).getStudentID().equals(mathList.get(z).getStudentID())){
 				if(historyList.get(i).getAbsences() > mathList.get(z).getAbsences()){
 					total ++;
